bc80113137c4fbbae0b56c4ad9ecf2d79e188797
David Parsley <parsley@linuxjedi.org>
2021-03-07 08:21:54 -0500

    Allow port arg in dev script

--- theia/run.sh
+++ theia/run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -p 127.0.0.1:3000:3000 --rm --name theia-base quay.io/lnxjedi/gopherbot-base-theia:latest
+
+LOCALPORT=${1:-3000}
+
+docker run -p 127.0.0.1:${LOCALPORT}:3000 --rm --name theia-base quay.io/lnxjedi/gopherbot-base-theia:latest
